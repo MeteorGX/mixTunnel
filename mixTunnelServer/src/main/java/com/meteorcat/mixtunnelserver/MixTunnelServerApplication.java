269289bef12eb340b6e1aeca87522ec90499ebac
MeteorCat <guixin2010@live.cn>
2022-11-03 01:16:56 +0800

    feat(initialize): 初始化项目
    配置项目初始化

---
+++ mixTunnelServer/src/main/java/com/meteorcat/mixtunnelserver/MixTunnelServerApplication.java
@@ -0,0 +1,13 @@
+package com.meteorcat.mixtunnelserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MixTunnelServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MixTunnelServerApplication.class, args);
+    }
+
+}
---
+++ mixTunnelServer/src/test/java/com/meteorcat/mixtunnelserver/MixTunnelServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meteorcat.mixtunnelserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MixTunnelServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
